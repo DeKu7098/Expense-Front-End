304fc718f2d75d749c1823645b67d3bd10ae1784
Arka <arkac720@gmail.com>
2022-06-26 17:08:42 +0530

    .map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './component/ExpenseItem';
+
 
 function App() {
+  const expenses = [
+    { 
+      date: new Date(2022, 5,20),
+      title: 'Food',
+      amount: 10,
+      locationOfExpenditure:'Food',
+      place:'KFC',
+    },
+    { 
+      date: new Date(2022, 5,21),
+      title : 'Petrol',
+      amount: 100,
+      locationOfExpenditure: 'Petrol',
+      place:'HP Petrol Pump',
+    },
+    {
+       date: new Date(2022, 5,22),
+       title: 'Movies',
+       amount: 200,
+       locationOfExpenditure: 'Movies',
+       place:'PvR',
+    },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { expenses.map((expense)=>{
+        return (
+          <ExpenseItem title={expense.title} 
+                   date={expense.date}
+                   amount={expense.amount}
+                   place={expense.place}
+                   location={expense.locationOfExpenditure}>
+          </ExpenseItem>
+        )
+      })
+    }
     </div>
+    
   );
-}
+    }
 
 export default App;
