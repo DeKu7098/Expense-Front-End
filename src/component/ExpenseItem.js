a5c3c1fd924a27fb5b99fc89f47d357f35798836
Arka <arkac720@gmail.com>
2022-06-29 23:54:21 +0530

    smaller component

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,48 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 
 function ExpenseItem(props) {
-//    const expenseDate = new Date(2022, 6, 22);
-//    const expenseTitle = 'Food';
-//    const expenseAmount = 10;
-
-//    const expenseDate2 = new Date(2022, 6, 22);
-//    const expenseTitle2 = 'Petrol';
-//    const expenseAmount2 = 100;
-
-//    const expenseDate3 = new Date(2022, 6, 22);
-//    const expenseTitle3 = 'Movies';
-//    const expenseAmount3 = 200;
-
-//    const locationOfExpenditure = 'Movies';
-//    const amountOfExpendature = 200;
-
-    return <div className="expense-item">
-        <div className="expense-item__description">
-        <div>{props.date.toDateString()}</div>
-            <h2>{props.title}</h2>
-            <div>{props.place}</div>
-            <div className="expense-item__price">Rs{props.amount}</div>
-           
-            {/* <div className="expense-item__price">Rs{props.amount}</div> */}
-            {/* <div className="expense-item__location">{props.location}</div> */}
-            
-        {/* <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">Rs{props.amount}</div>
-        <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">Rs{props.amount}</div> */}
+  return (
+       <div className="expense-item">
+        <ExpenseDate date={props.date} />
+       <ExpenseDetails title={props.title}
+                       amount={props.amount} />
         
-        {/* <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">Rs{props.amount}</div> */}
-        </div>
-        </div>
-        // </div>
-        // </div>
-        // </div>
-           
+       </div> 
+  );   
 }
 
 export default ExpenseItem;
