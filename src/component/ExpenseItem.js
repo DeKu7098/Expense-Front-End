27dcbd376f10f2e2fcc57010c4af31945965919d
Arka <arkac720@gmail.com>
2022-07-04 23:31:46 +0530

    arrow function

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,16 +1,16 @@
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+import Card from './Card';
 import './ExpenseItem.css';
 
-
-function ExpenseItem(props) {
+const  ExpenseItem = (props) => {
   return (
-       <div className="expense-item">
+       <Card className="expense-item">
         <ExpenseDate date={props.date} />
        <ExpenseDetails title={props.title}
                        amount={props.amount} />
         
-       </div> 
+       </Card> 
   );   
 }
 
