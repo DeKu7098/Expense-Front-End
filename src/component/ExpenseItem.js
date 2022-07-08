6ec031a2b8cb5eb88bbf6e003ba14dfc21cd756f
Arka <arkac720@gmail.com>
2022-07-08 13:34:34 +0530

    added expense amount change

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,22 +1,43 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from './Card';
 import './ExpenseItem.css';
 
 const  ExpenseItem = (props) => {
+   
+   const [title, setTitle] = useState(props.title);
+   const [amount, setAmount] = useState(props.amount);
+   
+   
+   
     const clickHandler = () =>{
-        console.log("Clicked!!!!!");
+
+        setTitle('Updated');
+        console.log(title);
     }
+    
+    
+    
     const deleteItem = () =>{
         console.log("Expense got Deleted");
     }
-  return (
+
+    const changeHandler = () =>{
+        setAmount('100');
+    }
+ 
+ 
+ 
+ 
+    return (
        <Card className="expense-item">
         <ExpenseDate date={props.date} />
-       <ExpenseDetails title={props.title}
-                       amount={props.amount} />
+       <ExpenseDetails title={title}
+                       amount={amount} />
         <button onClick={deleteItem}>Delete Expense</button>
        <button onClick={clickHandler}>Change Title</button>
+       <button onClick={changeHandler}> Change Expense Amount</button>
         
        </Card> 
   );   
