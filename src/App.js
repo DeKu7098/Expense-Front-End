e1fc7ab866ea462bba5f92f69f5e5fd561ff961c
Arka <arkac720@gmail.com>
2022-07-15 18:00:48 +0530

    showing Dynamic data

--- src/App.js
+++ src/App.js
@@ -1,59 +1,81 @@
+import React, { useState } from 'react';
 import ExpenseItem from './component/ExpenseItem';
 import './component/Expenses.css';
 import './component/Card';
 import Card from './component/Card';
 import NewExpense from './component/NewExpense';
+import ExpensesFilter from './component/ExpensesFilter';
+
+const DUMMY_EXPENSES = [
+  { 
+    id:"e1",
+    date: new Date(2022, 5,20),
+    title: 'Food',
+    amount: 10,
+    locationOfExpenditure:'Food',
+    place:'KFC',
+  },
+  { 
+    id:"e2",
+    date: new Date(2022, 5,21),
+    title : 'Petrol',
+    amount: 100,
+    locationOfExpenditure: 'Petrol',
+    place:'HP Petrol Pump',
+  },
+  {  
+    id:"e3",
+     date: new Date(2022, 5,22),
+     title: 'Movies',
+     amount: 200,
+     locationOfExpenditure: 'Movies',
+     place:'PvR',
+  },
+
+  {
+    id:"e4",
+    date: new Date(2022, 5,23),
+    title: 'Anime',
+    amount: 500,
+    locationOfExpenditure: 'Anime',
+    place:'Crunchyroll',
+ },
+];
 
 
 
 const App = () => {
-  const expenses = [
-    { 
-      date: new Date(2022, 5,20),
-      title: 'Food',
-      amount: 10,
-      locationOfExpenditure:'Food',
-      place:'KFC',
-    },
-    { 
-      date: new Date(2022, 5,21),
-      title : 'Petrol',
-      amount: 100,
-      locationOfExpenditure: 'Petrol',
-      place:'HP Petrol Pump',
-    },
-    {
-       date: new Date(2022, 5,22),
-       title: 'Movies',
-       amount: 200,
-       locationOfExpenditure: 'Movies',
-       place:'PvR',
-    },
+    
+  const [expenses, SetExpenses] = useState(DUMMY_EXPENSES);
+    
+  const addExpenseHandler = (expense) =>{
+     SetExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+     });
+ };
+ 
+ 
+ const [filteredYear, setFilteredYear] = useState('2020');
 
-    {
-      date: new Date(2022, 5,23),
-      title: 'Anime',
-      amount: 500,
-      locationOfExpenditure: 'Anime',
-      place:'Crunchyroll',
-   },
-  ];
- const addExpenseHandler = expense =>{
-  console.log('app');
-  console.log(expense);
- }
-
-  return (
+   const filterChangeHandler = (selectedYear) => {
+     setFilteredYear(selectedYear);
+   };
+ 
+ 
+   return (
     <Card>
       <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       { expenses.map((expense)=>{
         return (
-          <ExpenseItem title={expense.title} 
+          <ExpenseItem 
+                  key={expense.id}
+                  title={expense.title} 
                    date={expense.date}
                    amount={expense.amount}
                    place={expense.place}
-                   location={expense.locationOfExpenditure}>
-          </ExpenseItem>
+                   location={expense.locationOfExpenditure}
+          />
         )
       })
     }
